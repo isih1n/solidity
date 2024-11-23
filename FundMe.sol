e3bf9099302f4a492f2cb4ca544b654c4777e3ba
rizzzhul <rahulkumarmahto334@gmail.com>
2024-11-23 18:12:44 +0530

    contract for collecting funds

--- FundMe.sol
+++ FundMe.sol
@@ -4,22 +4,36 @@ pragma solidity 0.8.24;
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
 contract fundme{
-    uint256 public minimumUSD=5;
+    uint256 public minimumUSD=5e18;
+    address[] public funders;
+    mapping (address fuder=> uint256 amountFunded) public addressTOamountFunded;
 
     function fund() public payable {
-         require(msg.value > 1e18, "Require minimum 1eth for fund");       //1e18= 1 ETH = 1* 10 ** 18 wei
+         require(getConversionrate(msg.value) >= minimumUSD, "Require minimum 5usd for fund");       //1e18= 1 ETH = 1* 10 ** 18 wei
+         funders.push(msg.sender);
+         addressTOamountFunded[msg.sender]= addressTOamountFunded[msg.sender] + msg.value;
     }
 
-    function getPrice() public {
+    function getPrice() public view returns (uint256){
 
         //address: 0x694AA1769357215DE4FAC081bf1f309aDC325306
         //ABI:
-        AggregatorV3Interface PriceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        (,int256 answer,)=PriceFeed.latestRoundData();
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        (,int256 price,,,) = priceFeed.latestRoundData();
+
+        return uint256(price *1e10);
 
 
     }
-    function getConversionrate() public{}
+    function getConversionrate(uint256 ethAmount) public view returns (uint256){
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethAmount*ethPrice)/1e18;
+        return ethAmountInUsd;
+    }
+
+    function getVersion() public view returns(uint256){
+        return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version();
+    }
 
 }
 
