fd02d7f1c2fe1cb1716eb4e318bc13b96ad13593
rizzzhul <rahulkumarmahto334@gmail.com>
2024-11-25 15:40:40 +0530

    A contract for collecting funds and withdraw funds

--- FundMe.sol
+++ FundMe.sol
@@ -1,39 +1,63 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.24;
 
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import {PriceConverter} from "./PriceConverter.sol";
 
+error NotOwner();
+
+//799,727 gas without any constant anf immutable
+//779,358 gas with constant
 contract fundme{
-    uint256 public minimumUSD=5e18;
+    using PriceConverter for uint256; //this allows us to use the PriceConverter array
+    uint256 public constant MINIMUM_USD=5e18;
     address[] public funders;
-    mapping (address fuder=> uint256 amountFunded) public addressTOamountFunded;
+    mapping (address funder=> uint256 amountFunded) public addressTOamountFunded;
+
+    address public immutable  i_owner;
+
+    constructor() {
+        i_owner = msg.sender;
+    }
 
     function fund() public payable {
-         require(getConversionrate(msg.value) >= minimumUSD, "Require minimum 5usd for fund");       //1e18= 1 ETH = 1* 10 ** 18 wei
-         funders.push(msg.sender);
-         addressTOamountFunded[msg.sender]= addressTOamountFunded[msg.sender] + msg.value;
+         require(msg.value.getConversionrate() >= MINIMUM_USD, "Require minimum 5usd for fund");//1e18= 1 ETH = 1* 10 ** 18 wei
+         funders.push(msg.sender); //this stores the funder address to the funders array
+         addressTOamountFunded[msg.sender]= addressTOamountFunded[msg.sender] + msg.value; // this keeps updating the amount dinated by the funders
     }
 
-    function getPrice() public view returns (uint256){
+    function withdraw() public onlyOwner{
 
-        //address: 0x694AA1769357215DE4FAC081bf1f309aDC325306
-        //ABI:
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        (,int256 price,,,) = priceFeed.latestRoundData();
+        // loop for setting funders balance to zero as we call withdraw
+        for (uint256 funderIndex=0;funderIndex <funders.length;funderIndex++) 
+        {
+            //this line creates funder which stores addressof funders as they change per itteration 
+            address funder = funders[funderIndex];
+            addressTOamountFunded[funder]=0; // as funder sotres addresses and this line sets them to zero
+        }
 
-        return uint256(price *1e10);
-
-
+        funders= new address[](0); // we are using new keyword to creates a brand new address array compleletly reseting it
+        
+        //transfer
+        // payable (msg.sender).transfer(address(this).balance);
+        //send
+        // bool sendSucess =payable (msg.sender).send(address(this).balance);
+        // require(sendSucess, "send failed");
+        //call
+        (bool callSucess,)=msg.sender.call{value: address(this).balance}("");
+        require(callSucess,"call failed");
     }
-    function getConversionrate(uint256 ethAmount) public view returns (uint256){
-        uint256 ethPrice = getPrice();
-        uint256 ethAmountInUsd = (ethAmount*ethPrice)/1e18;
-        return ethAmountInUsd;
+
+    modifier onlyOwner(){
+        // require( msg.sender == i_owner ,"Withdraw function can omly be called by the owner of the contract");
+        if(msg.sender!= i_owner){revert NotOwner();}
+        _;
     }
 
-    function getVersion() public view returns(uint256){
-        return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version();
+    receive() external payable { 
+        fund();
     }
-
+    fallback() external payable {
+        fund();
+     }
 }
 
